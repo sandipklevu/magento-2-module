236db51356c8b0fa193475a8907801418909e312
sandip <sandip.chandela@klevu.com>
2023-10-17 14:36:36 +0530

    StopProductQueue module init

---
+++ Klevu/StopProductQueue/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Klevu Oy. All rights reserved. See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Klevu_StopProductQueue', __DIR__);
